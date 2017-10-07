5bbed2543952f72fe67f8709bf0c1ea998c327dc
Soptq <32592090+Soptq@users.noreply.github.com>
2017-10-07 18:35:52 +0800

    Soptq
    Now working on learning Android Studio

---
+++ SysUIRst/app/src/main/java/com/sysuirst/s0ptq/sysuirst/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.sysuirst.s0ptq.sysuirst;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Sopqt Created by S0ptq on 2017/10/3.
+ */
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
